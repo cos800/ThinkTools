a98a6c43a1096e3a86a444a77108045e5ec42c34
王忠强 <thisky@qq.com>
2018-04-09 18:30:04 +0800

    start

---
+++ src/think-tools/a.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: cos800
+ * Date: 2018/4/9
+ * Time: 下午6:01
+ */
+
+namespace tt;
+
+
+class a
+{
+    static function b() {
+        echo 'asd';
+    }
+}
---
+++ src/tt.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: cos800
+ * Date: 2018/4/9
+ * Time: 下午5:57
+ */
+
+class tt
+{
+    static function ff() {
+        echo 'ffff';
+    }
+}

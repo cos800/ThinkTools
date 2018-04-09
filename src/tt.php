d1b5ded61a41ceaeb62eebb444ad515dff0e3022
王忠强 <thisky@qq.com>
2018-04-09 19:51:10 +0800

    add tt::up()

--- src/tt.php
+++ src/tt.php
@@ -10,4 +10,45 @@ class tt
     static function ff() {
         echo 'ffff';
     }
+
+
+    static function up($path, $width=FALSE, $height=FALSE, $type=3) {
+        // 外链直接输出
+        if (!$path or !strncasecmp('http://', $path, 7)) {
+            return $path;
+        }
+        // 多图 处理第一张图片
+        if ($tmp = strpos($path, ',')) {
+            $path = substr($path, 0, $tmp);
+        }
+
+        if ($path{0}==='/') {
+            $uploadPath = '.';
+            $uploadUrl = '';
+        }else{
+            $uploadPath =  config('app.upload.root_path');
+            $uploadUrl = config('app.upload.base_url');
+        }
+        if ($width and $height) {
+            // 缩略图路径
+            $newPath = substr_replace($path, "_{$width}x{$height}", strrpos($path,'.'), 0);
+            $newRealPath = $uploadPath.$newPath; // 缩略图完整路径
+            $realPath = $uploadPath.$path; // 原图完成路径
+
+            // 不存在则生成缩略图
+            if (!is_file($newRealPath) and is_file($realPath)) {
+//            $Img = new Think\Image();
+//            $Img->open($realPath);
+                $Img = \think\Image::open($realPath);
+                try{
+                    $Img->thumb($width, $height, $type);
+                    $Img->save($newRealPath);
+                } catch (Exception $e) {
+//                echo 'Caught exception: ',  $e->getMessage(), "\n";
+                }
+            }
+            return $uploadUrl.$newPath;
+        }
+        return $uploadUrl.$path; // 返回原图URL
+    }
 }

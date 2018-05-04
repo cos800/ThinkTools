c6d6f20b8585acc7b9d4e76001c66d4ce71fa68c
王忠强 <thisky@qq.com>
2018-05-04 14:56:39 +0800

    no message

---
+++ src/think-tools/UploadController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * User: cos800
+ * Date: 2018/5/4
+ * Time: 下午2:45
+ */
+
+namespace tt;
+
+use think\facade\Request;
+
+class UploadController extends ApiBaseController
+{
+    function index() {
+        $subdir = Request::param('subdir');
+        $rootPath = config('app.upload.root_path');
+        if (!file_exists($rootPath.$subdir)) {
+            mkdir($rootPath.$subdir, 0777, true);
+        }
+
+
+        $file = request()->file('file');
+        $info = $file->validate([
+            'size' => config('app.upload.size'),
+            'ext' => config('app.upload.ext'),
+        ])->rule(function () {
+            return date('ym/d-His-').substr(microtime(),2,8);
+        })->move($rootPath.$subdir);
+
+        if ($info) {
+            $path = $subdir.'/'.$info->getSaveName();
+
+            $data = [
+                'path' => $path,
+                'url' => \tt::up($path),
+            ];
+
+            $this->success($data);
+        }else{
+            $this->error($file->getError());
+        }
+    }
+}
